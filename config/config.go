baa5d552c63f974837f5bb0989ac3b6a4367453c
Huang ChuanTong <chuantong.huang@gmail.com>
2014-08-18 21:04:26 +0800

    add log level support in config and start cmd line.
    args: -l [info|debug|warn|error]

--- config/config.go
+++ config/config.go
@@ -37,6 +37,7 @@ type Config struct {
 	Addr     string `yaml:"addr"`
 	User     string `yaml:"user"`
 	Password string `yaml:"password"`
+	LogLevel string `yaml:"log_level"`
 
 	Nodes []NodeConfig `yaml:"nodes"`
 
